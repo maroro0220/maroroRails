4ac3b0214bf626b9dc0e61b96cdfefd3790c61ca
maroro0220 <maro1339@gamil.com>
2018-01-03 08:50:40 +0000

    search & image

---
+++ instagram/app/controllers/posts_controller.rb
@@ -0,0 +1,63 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:read, :edit, :update, :destroy]
+  #befor_action은 하나의 액션을 실행하기 전에 해당 메소드를 실행
+  #only:안에 있는 거에서만 set_post를 실행하겠다.
+
+  def index
+    if params["post"].nil?
+      @posts=Post.all.reverse
+    else
+      @posts=Post.where("title LIKE ?", "%#{params["post"]["q"]}%")
+      #%붙여주면
+    end
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to '/'
+    puts '*****************************'
+    puts post_params
+    puts '*****************************'
+  end
+
+  def read
+    # @post=Post.find(params[:id])
+  end
+
+  def edit
+    # @post=Post.find(params[:id])
+  end
+
+  def update
+    # post=Post.find(params[:id])
+  # @post.update(title: params[:title], content: params[:content])
+  @post.update(post_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    # post=Post.find(params[:id])
+    @post.destroy
+    redirect_to '/'
+  end
+
+  private
+#private 위에는 public이라서 url로 요청오면 그것을 받아서 view를 보여준다.
+#private은 해당 컨트롤러(PostsController)에서만 사용 가능함.
+#객체에서 private 속성을 가지면 클래스 내부에서만 접근이 가능했었고, 이외의 상속 받은 클래스나 객체 인스턴스에서도 접근이 불가능했음.
+
+  def set_post
+    @post=Post.find(params[:id])
+  end
+
+  #strong parameter
+  #특정 파라미터의 특정 값만 쓸거야라고 해주는거
+  #실제로 쓸 파라미터들만 가져오는? 화이트리스트?
+  def post_params
+    params.require(:post).permit(:title,:content,:postimage,:q)
+  end
+
+end
--- instagram/config/routes.rb
+++ instagram/config/routes.rb
@@ -1,56 +1,23 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :blogs
+  root 'posts#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #rake route???
+  # resourece :posts #아래 주석하고 하면 내가 가진 경로들 다 볼 수 있음
+  # index
+  get '/posts' => 'posts#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #Create
+  get '/posts/new' => 'posts#new'
+  post '/posts' => 'posts#create'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #Read
+  get '/posts/:id' =>'posts#read'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #Update
+  get '/posts/:id/edit' =>'posts#edit'
+  put '/posts/:id' =>'posts#update'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  #Delete
+  delete '/posts/:id' => 'posts#destroy'
 end
